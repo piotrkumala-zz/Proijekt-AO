66a6a7187b59f1a1307f2dff284faa8d9dcc6bdb
Patryk Chodur <patrykchodur@icloud.com>
2020-02-08 19:37:48 +0100

    thresholding image

--- read_registration.m
+++ read_registration.m
@@ -2,5 +2,15 @@
 
 function result_string = read_registration(image)
 
+	% image need to be processed as an array of doubles
+	% but imread reads it as uint8
+	if (isa(image, 'uint8'))
+		image = double(image)/255.0;
+	end
+	
+	threshold = 0.5;
+	image = rgb2gray(image);
+	image = imbinarize(image, threshold);
+
 	result_string = "test";
 end
